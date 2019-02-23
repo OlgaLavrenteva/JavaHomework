d509b6ee3cdbee765ef0a44bdf0f4b468f27d7ad
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2019-02-23 14:53:06 +0300

    Added possibility to add new note. Technical methods to search for free array cell and to extend the array are implemented.

--- Notebook/src/Notebook.java
+++ Notebook/src/Notebook.java
@@ -1,34 +1,70 @@
 public class Notebook {
     private String notebookName;
-    private int numberOfNotes;
+    private int sizeOfNotebook;
+    //private int numberOfNotes;
     private Note[] notes;
 
     public Notebook()
     {
         this.notebookName = "A name is not assigned";
-        this.numberOfNotes = 100;
-        this.notes = new Note[this.numberOfNotes];
+        this.sizeOfNotebook = 100;
+        this.notes = new Note[this.sizeOfNotebook];
     }
 
     public Notebook(String notebookName)
     {
         this.notebookName = notebookName;
-        this.numberOfNotes = 100;
-        this.notes = new Note[this.numberOfNotes];
+        this.sizeOfNotebook = 100;
+        this.notes = new Note[this.sizeOfNotebook];
     }
 
-    public Notebook(int numberOfNotes)
+    public Notebook(int initialSizeOfNotebook)
     {
         this.notebookName = "A name is not assigned";
-        this.numberOfNotes = numberOfNotes;
-        this.notes = new Note[this.numberOfNotes];
+        this.sizeOfNotebook = initialSizeOfNotebook;
+        this.notes = new Note[this.sizeOfNotebook];
     }
 
-    public Notebook(String notebookName, int numberOfNotes)
+    public Notebook(String notebookName, int initialSizeOfNotebook)
     {
         this.notebookName = notebookName;
-        this.numberOfNotes = numberOfNotes;
-        this.notes = new Note[this.numberOfNotes];
+        this.sizeOfNotebook = initialSizeOfNotebook;
+        this.notes = new Note[this.sizeOfNotebook];
     }
 
+    public void addNote(String newNote)
+    {
+        if (null == newNote)
+        {
+            System.out.println("Empty note cannot be added");
+        }
+        else
+        {
+            int indexForAdd = indexFreePlace();
+            notes[indexForAdd] = new Note(newNote);
+        }
+    }
+
+    private int indexFreePlace()
+    {
+        for (int noteIndex = 0; noteIndex < sizeOfNotebook; noteIndex++)
+        {
+            if (null == notes[noteIndex])
+            {
+                return noteIndex;
+            }
+        }
+
+        int indexOfExtendStart = sizeOfNotebook;
+        extendNotebook();
+        return indexOfExtendStart;
+    }
+
+    private void extendNotebook()
+    {
+        int extendedSizeOfNotebook = (int) (sizeOfNotebook*0.5);
+        Note[] extendedNotes = new Note[extendedSizeOfNotebook];
+        System.arraycopy(notes,0, extendedNotes,0,sizeOfNotebook);
+        notes = extendedNotes;
+    }
 }
