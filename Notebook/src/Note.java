0f1f0b6fd7fcbd245d746fbec3cf2f3229e6df1c
OlgaLavrenteva <olga.a.lavrentieva@gmail.com>
2019-02-23 14:07:16 +0300

    Constructors are implemented Setter and getter for Note are implemented

--- Notebook/src/Note.java
+++ Notebook/src/Note.java
@@ -1,2 +1,18 @@
 public class Note {
+    private String record;
+
+    public Note(String record)
+    {
+        this.record = record;
+    }
+
+    public void set(String record)
+    {
+        this.record = record;
+    }
+
+    public String get()
+    {
+        return this.record;
+    }
 }
--- Notebook/src/Notebook.java
+++ Notebook/src/Notebook.java
@@ -1,2 +1,34 @@
 public class Notebook {
+    private String notebookName;
+    private int numberOfNotes;
+    private Note[] notes;
+
+    public Notebook()
+    {
+        this.notebookName = "A name is not assigned";
+        this.numberOfNotes = 100;
+        this.notes = new Note[this.numberOfNotes];
+    }
+
+    public Notebook(String notebookName)
+    {
+        this.notebookName = notebookName;
+        this.numberOfNotes = 100;
+        this.notes = new Note[this.numberOfNotes];
+    }
+
+    public Notebook(int numberOfNotes)
+    {
+        this.notebookName = "A name is not assigned";
+        this.numberOfNotes = numberOfNotes;
+        this.notes = new Note[this.numberOfNotes];
+    }
+
+    public Notebook(String notebookName, int numberOfNotes)
+    {
+        this.notebookName = notebookName;
+        this.numberOfNotes = numberOfNotes;
+        this.notes = new Note[this.numberOfNotes];
+    }
+
 }
