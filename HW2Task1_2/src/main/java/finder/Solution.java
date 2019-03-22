f5a01e6b8aeec4300c295a1d64e172e748fade3b
Olga_Lavrenteva <olga.a.lavrentieva@gmail.com>
2019-03-22 16:04:05 +0300

    Added file for analyzing

--- HW2Task1_2/src/main/java/finder/Solution.java
+++ HW2Task1_2/src/main/java/finder/Solution.java
@@ -3,7 +3,7 @@ package main.java.finder;
 public class Solution {
     public static void main(String[] args){
         String currentDir = System.getProperty("user.dir");
-        String inputFile = currentDir + "\\HW2Task1_2\\src\\test\\java\\finder\\FileForAnalyzing1";
+        String inputFile = currentDir + "\\HW2Task1_2\\src\\test\\java\\finder\\FileForAnalyzing1.java";
         String outputFileBytes = currentDir + "\\out\\test_bytes.txt";
         String outputFileSymbols = currentDir + "\\out\\test_symbols.txt";
         JavaKeyWordsFinderBytesIO.findJavaKeyWords(inputFile, outputFileBytes);
