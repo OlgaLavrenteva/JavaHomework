c08d3d8ad35a24f0e9d87336c7dd93882be5e8da
Olga_Lavrenteva <olga.a.lavrentieva@gmail.com>
2019-03-24 21:55:47 +0300

    Class Film is implemented

---
+++ HW2Task3/src/main/java/films_collection/Film.java
@@ -0,0 +1,61 @@
+package main.java.films_collection;
+
+import java.util.HashSet;
+
+public class Film {
+    private String title;
+    private HashSet<Actor> actors;
+
+    public Film(String title) throws Exception{
+        setTitle(title);
+    }
+
+    public Film(HashSet<Actor> actors) throws Exception{
+        setActors(actors);
+    }
+
+    public Film(Actor actor) throws Exception{
+        setActors(actor);
+    }
+
+    public void setTitle(String title) throws Exception{
+        if(title==null){
+            throw new Exception("Film title is null.");
+        }
+        this.title=title;
+    }
+
+    public void setActors(HashSet<Actor> actors) throws Exception{
+        if(actors==null){
+            throw new Exception("Actors are null.");
+        }
+        this.actors=actors;
+    }
+
+    public void setActors(Actor actor) throws Exception{
+        if(actor==null){
+            throw new Exception("Actor is null.");
+        }
+        this.actors.add(actor);
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public HashSet<Actor> getActors(){
+        return actors;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder film = new StringBuilder("Film title: ");
+        film.append(title);
+        film.append(" Actors:");
+        for (Actor actor: actors){
+            film.append(" ");
+            film.append(actor.toString());
+        }
+        return film.toString();
+    }
+}
